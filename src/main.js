41d87eeba640ac97132b3e22cce2244405da81d9
fosong98 <fosong98@gmail.com>
2024-05-09 19:52:18 +0900

    :sparkles:feat: 여행경로 상세 페이지 지도 디자인 적용

--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@ import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
+import { useKakao } from 'vue3-kakao-maps/@utils';
+
+useKakao('8e8f1a95804db62ccd2f966948285d2b')
 
 app.use(createPinia());
 app.use(router)
