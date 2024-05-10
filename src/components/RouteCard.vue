a6f23f1cd29e414b3292f16b7c6faf74d2f4b4e3
jaedoo <fosong98@gmail.com>
2024-05-10 09:27:07 +0900

    :boom:change: 여행경로 목록 페이지에서 상세 페이지 이동 방법 수정

--- src/components/RouteCard.vue
+++ src/components/RouteCard.vue
@@ -8,29 +8,26 @@ let date = computed(() => {
   return dt.getFullYear()+'-'+(dt.getMonth()+1)+'-'+dt.getDate();
 })
 
-let moveDetailPage = (route) => {
+let moveDetailPage = () => {
   router.push("/routes/" + route.routeId)
 }
 </script>
 
 <template>
   <div class="card my-5 d-flex flex-row align-items-center">
-    <div id="thumbnail" class="rounded-1 h-100" :style="{ backgroundImage: `url(${route.thumbnail})` }"></div>
-    <div class="h-100 ms-5">
+    <div id="thumbnail" class="rounded-1 h-100" :style="{ backgroundImage: `url(${route.thumbnail})` }" @click="moveDetailPage"></div>
+    <div class="h-100 ms-5" @click="moveDetailPage">
       <p class="fs-4 fw-bold">{{ route.title  }}</p>
       <p class="date">{{ date }} ~</p>
     </div>
-    <div class="h-100 align-items-center ms-5 w-50 overflow-y-auto overview px-2">
+    <div class="h-100 align-items-center ms-5 w-50 overflow-y-auto overview px-2" @click="moveDetailPage">
       <p>{{ route.overview }}</p>
     </div>
     <div class="dropdown-center ms-auto me-5">
       <h2 class="bi bi-three-dots" data-bs-toggle="dropdown" aria-expanded="false"></h2>
       <ul class="dropdown-menu">
-        <li><a class="dropdown-item" @click="moveDetailPage(route)">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
-        <li><hr class="dropdown-divider"></li>
-        <li><a class="dropdown-item" href="#">Separated link</a></li>
+        <li><a class="dropdown-item">Edit</a></li>
+        <li><a class="dropdown-item">Delete</a></li>
       </ul>
     </div>
   </div>
---
+++ src/components/RouteListView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import RouteCard from '@/components/RouteCard.vue'
+
+let { routeList } = defineProps({ routeList: Object });
+
+</script>
+
+<template>
+  <div class="container px-3" id="tripList">
+    <RouteCard v-for="route in routeList" :key="route.routeId" :route="route" />
+  </div>
+</template>
+
+<style scoped>
+#tripList {
+  height: 70vh;
+  overflow-y: scroll;
+}
+
+#tripList::-webkit-scrollbar {
+  width: 5px;
+}
+#tripList::-webkit-scrollbar-track {
+  background-color: white;
+}
+#tripList::-webkit-scrollbar-thumb {
+  background-color: black;
+}
+#tripList::-webkit-scrollbar-button {
+  display: none;
+}
+</style>
