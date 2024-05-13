80bbe1c31442a7bff1820e5dd3debca047654185
jaedoo <fosong98@gmail.com>
2024-05-13 10:37:56 +0900

    :sparkles: feat: 여행지 검색 결과 리스트 뷰 디자인

---
+++ src/components/route/SearchedAttractionListView.vue
@@ -0,0 +1,35 @@
+<script setup>
+import AttractionCard from '@/components/route/AttractionCard.vue'
+
+let { attractionList } = defineProps({ attractionList: Object })
+let emits = defineEmits(["moveMarker"])
+
+const moveMarker = (obj) => {
+  emits("moveMarker", obj.latitude, obj.longitude)
+}
+</script>
+
+<template>
+  <AttractionCard v-for="(attraction, index) in attractionList" :key="index" :attraction="attraction" @click="moveMarker(attraction)" />
+  <nav class="ms-auto w-100 my-5">
+    <ul class="pagination">
+      <li class="page-item ms-auto">
+        <a class="page-link" href="#" aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>
+      <li class="page-item"><a class="page-link active" href="#">1</a></li>
+      <li class="page-item"><a class="page-link" href="#">2</a></li>
+      <li class="page-item"><a class="page-link" href="#">3</a></li>
+      <li class="page-item me-auto">
+        <a class="page-link" href="#" aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
